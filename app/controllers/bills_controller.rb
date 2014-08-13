99ed1306479db119ef881a928fe8ecde5d55735a
Neson <neson@dex.tw>
2014-08-14 01:10:00 +0800

    debug user first_or_create not saving on new bill

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -46,7 +46,7 @@ class BillsController < ApplicationController
 
       params['payers'].each do |payer_fbid|
         pc += 1
-        @payer = User.where({:fbid => payer_fbid}).first_or_create
+        @payer = User.where({:fbid => payer_fbid}).first_or_create!
         @payers << @payer
         for i in 1..params['bill'].length
           @bill = current_user.charges.build(params.require('bill').require((i-1).to_s).permit(:name, :description, :unit_price, :quantity, :total_price))
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   end
 
   def self.from_facebook(auth)
-    user = where({:fbid => auth.uid}).first_or_create do |user|
+    user = where({:fbid => auth.uid}).first_or_create! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
