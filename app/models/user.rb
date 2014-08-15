5462dba91b276db448033f6479f8bea06f989f85
Neson <neson@dex.tw>
2014-08-15 21:10:56 +0800

    add overview on menu top

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :bills, class_name: "Bill", foreign_key: "payer_id"
 
   has_many :debtors, -> { where(["state = ? or state = ? or state = ? or state = ?", 'New', 'Accepted', 'Dispute', 'Paid?']).uniq }, through: :charges, source: :payer
-  has_many :creditors, -> { where(["state = ? or state = ? or state = ?", 'New', 'Accepted', 'Dispute']).uniq }, through: :bills, source: :payee
+  has_many :creditors, -> { where(["state = ? or state = ? or state = ? or state = ?", 'New', 'Accepted', 'Dispute', 'Paid?']).uniq }, through: :bills, source: :payee
   has_many :recent_debtors, -> { where(["state = ? or state = ? or state = ? or state = ? or bills.updated_at > ?", 'New', 'Accepted', 'Dispute', 'Paid?', (Time.now - 7.days)]).uniq }, through: :charges, source: :payer
   has_many :recent_creditors, -> { where(["state = ? or state = ? or state = ? or bills.updated_at > ?", 'New', 'Accepted', 'Dispute', (Time.now - 7.days)]).uniq }, through: :bills, source: :payee
 
